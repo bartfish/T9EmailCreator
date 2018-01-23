102137709d5d2a908da2e1ba88a0ae3a851db194
Bart <bartlomiej.ryba@interia.pl>
2018-01-23 12:48:47 +0100

    Small Trie Update

--- T9/T9/Email.h
+++ T9/T9/Email.h
@@ -4,37 +4,37 @@
 // no limit for the number of signs
 class Email : public Message
 {
-private:
-	Trie Emails;
-	string invalid_email_message;
-	unsigned int priority_type;
-	enum Priority : unsigned int
-	{
-		HIGH = 1,
-		NORMAL = 2,
-		LOW = 3
-	};
+	private:
+		Trie Emails;
+		string invalid_email_message;
+		unsigned int priority_type;
+		enum Priority : unsigned int
+		{
+			HIGH = 1,
+			NORMAL = 2,
+			LOW = 3
+		};
 
 
-public:
-	Email() : 
-		Emails(""),
-		priority_type(NORMAL),
-		invalid_email_message("Please, enter valid email address(e.x. example@example.com)") 
-	{}
-	~Email() {}
+	public:
+		Email() : 
+			Emails(""),
+			priority_type(NORMAL),
+			invalid_email_message("Please, enter valid email address(e.x. example@example.com)") 
+		{}
+		~Email() {}
 
-	bool is_valid_recepient_info(const string &s);
+		bool is_valid_recepient_info(const string &s);
 
-	void set_recipient(const string &s);
-	void set_topic(const string &s);
-	void set_message_content(const string &s);
+		void set_recipient(const string &s);
+		void set_topic(const string &s);
+		void set_message_content(const string &s);
 
-	string assign_message_content();
+		string assign_message_content();
 
-	void inputting_contact_info() {};
-	void load_contacts();
-	vector<string> load_all_words_from_contacts(const string & s);
-	void create_message(myConsoleManager Con);
+		void inputting_contact_info() {};
+		void load_contacts();
+		vector<string> load_all_words_from_contacts(const string & s);
+		void create_message(myConsoleManager Con);
 
 };
--- T9/T9/Trie.h
+++ T9/T9/Trie.h
@@ -1,15 +1,15 @@
 #pragma once
 #include "libraries.h"
 class Trie {
-public:
-	map<char, Trie> children;
-	string value;
-	bool is_end_of_word; // for loading words from trie (load when the bool is true)
+	public:
+		map<char, Trie> children;
+		string value;
+		bool is_end_of_word; // for loading words from trie (load when the bool is true)
 
-	Trie(const string &);
-	void add(char c);
-	void insert(const string & word);
+		Trie(const string &);
+		void add(char c);
+		void insert(const string & word);
 
-	vector<string> all_prefixes();
-	vector<string> load_all_words(const string & s);
+		vector<string> all_prefixes();
+		vector<string> load_all_words(const string & s);
 };
